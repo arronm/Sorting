da50a1362fa9f5edc11314d76b4d1cba03148653
Arron M <marshall.arron@gmail.com>
2019-09-09 15:00:08 -0700

    Implement count_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,21 +18,41 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     swapped = 1
-    iterations = 0
     while swapped > 0:
         swapped = 0
-        for index, item in enumerate(arr, start=0):
-            iterations += 1
-            if index >= len(arr) - 1:
-                break
+        for index in range(0, len(arr) - 1):
             if arr[index] > arr[index + 1]:
                 swapped += 1
                 arr[index], arr[index + 1] = arr[index + 1], arr[index]
-    print(f"{iterations} iterations")
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    # Get largest number
+    largest_num = 0
+    sorted_arr = [0] * len(arr)
 
-    return arr
+    # Find the largest Number
+    for index in range(0, len(arr) - 1):
+        if arr[index] < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        if arr[index] > largest_num:
+            largest_num = arr[index]
+    
+    # Create list of number indexes
+    num_list = [0] * (largest_num + 1)
+
+    # Count our numbers
+    for num in arr:
+        num_list[num] += 1
+
+    # fill out our sorted array
+    cur_index = 0
+    for num in range(0, len(num_list)):
+        while num_list[num] > 0:
+            sorted_arr[cur_index] = num
+            num_list[num] -= 1
+            cur_index += 1
+
+    return sorted_arr
